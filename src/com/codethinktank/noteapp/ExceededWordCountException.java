a9eb86a6cfa843a9294ef3ee63d9830c6981cc83
Anand Mohan <im.anand.m@gmail.com>
2020-04-03 17:23:56 +0530

    Added String pooling example

--- src/com/codethinktank/noteapp/ExceededWordCountException.java
+++ src/com/codethinktank/noteapp/ExceededWordCountException.java
@@ -8,6 +8,4 @@ public class ExceededWordCountException extends Exception {
 
     }
 
-
-
 }
---
+++ src/com/codethinktank/stringdemo/Main.java
@@ -0,0 +1,20 @@
+package com.codethinktank.stringdemo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = "Abdul";
+
+        String newname = name.concat(" Kalam");
+
+        System.out.println(name);
+
+        System.out.println(newname);
+
+
+
+    }
+
+
+}
